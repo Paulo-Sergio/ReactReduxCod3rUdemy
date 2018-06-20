97a4f6763fb09f6ccdcaf01a6b7a312dd45ac57a
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-06-20 10:38:07 -0300

    criando estrutura inicial do projeto React

---
+++ todo-app/frontend/src/main/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Teste</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
