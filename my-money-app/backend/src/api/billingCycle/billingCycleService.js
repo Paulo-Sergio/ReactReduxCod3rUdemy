45c98e657572d7d5d8c067b77d41f9ca6aa02d7f
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-07-04 10:57:25 -0300

    Obtendo a Qtde de Registros (Count) - Obtendo o Sumário de Pagamentos

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,37 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // para RETORNAR OBJ ATUALIZADO e validar no verbo HTTP PUT
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+// nova rota de count, não precisa fazer o 'register', pois já é feito routes.js (http://localhost:3003/api/billingCycles/count)
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
+// nova rota de summary, não precisa fazer o 'register', pois já é feito routes.js (http://localhost:3003/api/billingCycles/summary)
+BillingCycle.route('summary', (req, res, next) => {
+  BillingCycle.aggregate(
+    {
+      $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    },
+    {
+      $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    },
+    {
+      $project: { _id: false, credit: true, debt: true }
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json({ erros: [error] })
+      } else {
+        res.json(result[0] || { credit: 0, debt: 0 })
+      }
+    }
+  )
+})
+
 module.exports = BillingCycle
